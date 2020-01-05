7988f15ee6a99eefbebfecb3514a28565f276587
wangkezheng <191524234>
2020-01-05 22:16:59 +0800

    aa

--- demo/src/main/java/com/example/demo/java/cool/niubi.java
+++ demo/src/main/java/com/example/demo/java/cool/niubi.java
@@ -6,7 +6,6 @@ public class niubi {
     }
 
     public void buniubi() {
-        String aa = "aa";
         System.out.println("aabbc");
         System.out.println("sdfads");
         System.out.println("asdass");
