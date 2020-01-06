c0b672f1d03c443d9ad8d9f4b259fc6b8f8864a4
wangkezheng <191524234>
2020-01-06 09:35:27 +0800

    实验

--- demo/src/main/java/com/example/demo/java/cool/niubi.java
+++ demo/src/main/java/com/example/demo/java/cool/niubi.java
@@ -6,8 +6,9 @@ public class niubi {
     }
 
     public void buniubi() {
+        String aa = "aa";
         System.out.println("aabbc");
-        System.out.println("sdfads");
+        System.out.println("sdfadsaa");
         System.out.println("asdass");
     }
 }
